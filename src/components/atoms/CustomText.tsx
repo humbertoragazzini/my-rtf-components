c7aff57ca03d53f155ebe14fe05a0a0d65138ff8
humbertoragazzini <beto_vicio@hotmail.com>
2026-03-09 11:32:51 +0000

    adde value to test

--- src/components/atoms/CustomText.tsx
+++ src/components/atoms/CustomText.tsx
@@ -4,17 +4,17 @@ import { DivContext } from "./Div";
 
 export default function CustomText() {
 
-      const context = useContext(DivContext)
+    const context = useContext(DivContext)
 
-  if (!context) {
-    throw new Error('CustomText must be used inside <Div>')
-  }
+    if (!context) {
+        throw new Error('CustomText must be used inside <Div>')
+    }
 
-  const { width } = context
+    const { width } = context
 
     return (
         <Text maxWidth={width} fontSize={1} color="white">
-            Hello World
+            Hello World {width}
         </Text>
     )
 }
