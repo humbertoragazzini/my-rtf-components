3a9d608b3c3a48939ae5cf77625863baa7f2705d
humbertoragazzini <beto_vicio@hotmail.com>
2026-03-09 11:22:40 +0000

    added width

--- src/components/atoms/CustomText.tsx
+++ src/components/atoms/CustomText.tsx
@@ -1,8 +1,13 @@
 import { Text } from "@react-three/drei";
+import { useContext } from "react";
+import { DivContext } from "./Div";
 
 export default function CustomText() {
+
+    const { width } = useContext(DivContext)
+
     return (
-        <Text>
+        <Text maxWidth={width} fontSize={1} color="white">
             Hello World
         </Text>
     )
