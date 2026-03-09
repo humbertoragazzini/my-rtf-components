f436a588bdb7a43a7539f0e9b2287bfa4672ba1a
humbertoragazzini <beto_vicio@hotmail.com>
2026-03-09 11:46:23 +0000

    detached ligth from canvas

--- src/components/atoms/CustomDirectionalLight.tsx
+++ src/components/atoms/CustomDirectionalLight.tsx
@@ -1,4 +1,8 @@
 import { useControls } from 'leva';
+import { useRef } from 'react';
+import { useHelper } from '@react-three/drei';
+import { DirectionalLightHelper } from 'three';
+import * as THREE from 'three';
 
 type DirectionalLightProps = {
     name?: string;
@@ -9,13 +13,18 @@ type DirectionalLightProps = {
 
 export const CustomDirectionalLight = ({ name = 'Directional Light', intensity = 1, color = '#ffffff', position = [10, 10, 5] }: DirectionalLightProps) => {
 
+    const lightRef = useRef<THREE.DirectionalLight>(null!);
+
     const tweaks = useControls(name, {
         intensity: { value: intensity, min: 0, max: 5, step: 0.1 },
         color: color,
-        position: position
+        position: position,
+        showHelper: { value: true, label: 'Show Helper' }
     });
 
+    useHelper(tweaks.showHelper && lightRef, DirectionalLightHelper, 1, tweaks.color);
+
     return (
-        <directionalLight intensity={tweaks.intensity} color={tweaks.color} position={tweaks.position} />
+        <directionalLight ref={lightRef} intensity={tweaks.intensity} color={tweaks.color} position={tweaks.position} />
     );
 };
